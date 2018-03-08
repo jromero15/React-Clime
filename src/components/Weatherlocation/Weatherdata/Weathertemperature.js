5e5c6d46d1e6cebd45b2a44d4a764d99c6d3be6b
Jonathan Romero <jromero0527@gmail.com>
2018-03-08 16:34:02 -0500

    Version 2.5.3

--- src/components/Weatherlocation/Weatherdata/Weathertemperature.js
+++ src/components/Weatherlocation/Weatherdata/Weathertemperature.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
-import {CLOUD, CLOUDY, SUN, RAIN} from './../constants/weathers';
+import {CLOUD, CLOUDY, SUN, RAIN} from './../../../constants/weathers';
 import PropTypes from 'prop-types';
 
 const stataToIconName = weatherState => {
--- src/components/Weatherlocation/Weatherdata/index.js
+++ src/components/Weatherlocation/Weatherdata/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Weathertemperature from './Weathertemperature';
 import Weatherextrainfo from './Weatherextrainfo';
-import { CLOUD, CLOUDY, SUN, RAIN } from './../constants/weathers';
+import { CLOUD} from './../../../constants/weathers';
+import 'Style.css';
 
 const Weatherdata = () => (
-    <div>
-        <Weathertemperature temperature= {32} weatherState= {RAIN}/>
+    <div clas name='prueba'>
+        <Weathertemperature temperature= {32} weatherState= {CLOUD}/>
         <Weatherextrainfo humidity={80} wind={'10 m/s'} />
     </div>
 );
